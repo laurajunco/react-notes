4e20fd03d0a7eea55d3cae80361a17e4050a2762
Laura Junco <lc.junco10@uniandes.edu.co>
2020-08-04 11:57:10 -0500

    react-app

---
+++ helloworld/src/HelloWorld.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./HelloWorld.css";
+
+class HelloWorld extends React.Component {
+    render() {
+        return (
+            <div className="helloContainer">
+                <h1>Hello, world!</h1>
+            </div>
+        );
+    }
+}
+
+export default HelloWorld;
