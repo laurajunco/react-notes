4c771c78c3e2814dc696701ab15f5696527a980f
Laura Junco <lc.junco10@uniandes.edu.co>
2020-08-05 16:40:18 -0500

    Update method

--- d3/03_barChart/src/D3Chart.js
+++ d3/03_barChart/src/D3Chart.js
@@ -12,72 +12,80 @@ const height = 500 - margin.top - margin.bottom;
 
 class D3Chart {
 
-    //recibe el elemento contenedor
+    //cosas que no cambian
     constructor(element) {
+        const vis = this;
 
-        const svg = d3.select(element)
+        vis.svg = d3.select(element)
             .append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
             .attr("transform", `translate(${margin.left}, ${margin.top})`)
 
+        // draw Axis labels
+        vis.xLabel = this.svg.append("text")
+            .attr("x", width / 2)
+            .attr("y", height + 50)
+            .attr("text-anchor", "middle")
+            .text("The worlds tallest men")
 
+        vis.yLabel = this.svg.append("text")
+            .attr("x", -(height / 2))
+            .attr("y", -50 )
+            .attr("text-anchor", "middle")
+            .attr("transform", "rotate(-90)")
+            .text("Height in cm");
 
+        vis.xAxisGroup = this.svg.append("g")
+            .attr("transform", `translate(0, ${height})`);
+
+        vis.yAxisGroup = this.svg.append("g");
+            
         d3.json(url).then(dataset => {
-            console.log(dataset);
-
-            //scales
-            var y = d3.scaleLinear()
-                .domain([ 
-                    d3.min(dataset, d => d.height) * 0.95,
-                    d3.max(dataset, d => d.height)])
-                .range([height, 0]);
-
-            var x = d3.scaleBand()
-                .domain(dataset.map(d => d.name))
-                .range([0, width])
-                .padding(0.3);
-
-            //axis generators
-            var xAxisCall = d3.axisBottom(x)
-            var yAxisCall = d3.axisLeft(y);
+            vis.dataset = dataset;
             
-            //rects
-            var rects = svg.selectAll("rect")
-                .data(dataset)
-                .enter()
-                .append("rect")
-                .attr("x", d => x(d.name))
-                .attr("y", d => y(d.height))
-                .attr("width", x.bandwidth)
-                .attr("height", d => height - y(d.height))
-                .attr("fill", "grey");
-
-            //drawAxis
-            var xAxis = svg.append("g")
-                .attr("transform", `translate(0, ${height})`)
-                .call(xAxisCall);
-
-            var yAxis = svg.append("g")
-                .call(yAxisCall);
-
-            
-            // draw Axis labels
-            var xLabel = svg.append("text")
-                .attr("x", width / 2)
-                .attr("y", height + 50)
-                .attr("text-anchor", "middle")
-                .text("The worlds tallest men")
-
-            var yLabel = svg.append("text")
-                .attr("x", -(height / 2))
-                .attr("y", -50 )
-                .attr("text-anchor", "middle")
-                .attr("transform", "rotate(-90)")
-                .text("Height in cm")
+            d3.interval(() => {
+                vis.update();
+            }, 1000);
 
         })
+    }
+
+    //cosas que se ctualizan
+    update () {
+        const vis = this;
+
+        //scales
+        var y = d3.scaleLinear()
+        .domain([ 
+            d3.min(vis.dataset, d => d.height) * 0.95,
+            d3.max(vis.dataset, d => d.height)])
+        .range([height, 0]);
+
+        var x = d3.scaleBand()
+            .domain(vis.dataset.map(d => d.name))
+            .range([0, width])
+            .padding(0.3);
+
+        //axis generators
+        var xAxisCall = d3.axisBottom(x);
+        var yAxisCall = d3.axisLeft(y);
+
+        //rects
+        var rects = vis.svg.selectAll("rect")
+            .data(vis.dataset)
+            .enter()
+            .append("rect")
+            .attr("x", d => x(d.name))
+            .attr("y", d => y(d.height))
+            .attr("width", x.bandwidth)
+            .attr("height", d => height - y(d.height))
+            .attr("fill", "grey");
+
+        //drawAxis
+        vis.xAxisGroup.call(xAxisCall); 
+        vis.yAxisGroup.call(yAxisCall);
     }
 }
 
