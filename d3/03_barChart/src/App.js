7f6f0c53588475727e6cb7562ca8a00a5200b81e
Laura Junco <lc.junco10@uniandes.edu.co>
2020-08-06 11:14:27 -0500

    Add dropdown

--- d3/03_barChart/src/App.js
+++ d3/03_barChart/src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import ChartWrapper from './ChartWrapper';
+import GenderDropdown from './GenderDropdown';
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class App extends Component {
+  state = {
+    gender: "men"
+  };
+
+  genderSelected = (gender) => {
+    this.setState ({
+      gender: gender
+    });
+  };
+
   render() {
     return (
       <div className="App">
@@ -13,7 +26,16 @@ class App extends Component {
         </Navbar>
 
         <Container>
-          <ChartWrapper />
+          <Row>
+            <Col xs={12}>
+              <GenderDropdown genderSelected={this.genderSelected}/>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <ChartWrapper />
+            </Col>
+          </Row>
         </Container>
         
       </div>
